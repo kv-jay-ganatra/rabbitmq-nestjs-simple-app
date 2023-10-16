e1c23397230d9b268294f2759b6fdff2702898a5
Jay Ganatra <jay.ganatra@kevit.io>
2023-10-16 18:53:04 +0530

    init code

---
+++ microservice-client/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  try{
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }catch(e)
+  {
+    console.log(e)
+  }
+}
+bootstrap();
